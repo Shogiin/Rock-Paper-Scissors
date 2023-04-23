aafbe6c55802970c1c29ecf2ed3d13735bb936bb
Shogiin <wood69330@gmail.com>
2023-04-23 19:43:18 +0100

    Added prompt for answer and alert to display win/loss/tie

--- script.js
+++ script.js
@@ -11,34 +11,36 @@ let computerScore = 0 // Computer Score
 // Play a round between Player & computer
 function playRound(playerSelection, computerSelection) {
     if (playerSelection === "rock" && computerSelection === "rock") {
-        console.log("It's a tie!") 
+    alert("It's a tie!") 
     } else if (playerSelection === "paper" && computerSelection === "paper") {
-        console.log("It's a tie!") 
+    alert("It's a tie!") 
     } else if (playerSelection === "scissors" && computerSelection === "scissors") {
-        console.log("It's a tie!") 
+    alert("It's a tie!") 
     } else if (playerSelection === "rock" && computerSelection === "paper") {
         computerScore++;
-        console.log("Computer Wins!") 
+    alert("Computer Wins!") 
     } else if (playerSelection === "paper" && computerSelection === "scissors") {
         computerScore++;
-        console.log("Computer Wins!") 
+    alert("Computer Wins!") 
     } else if (playerSelection === "scissors" && computerSelection === "rock") {
         computerScore++;
-        console.log("Computer Wins!") 
+    alert("Computer Wins!") 
     } else if (playerSelection === "rock" && computerSelection === "scissors") {
         playerScore++;
-        console.log("You Win!") 
+    alert("You Win!") 
     } else if (playerSelection === "scissors" && computerSelection === "paper") {
         playerScore++;
-        console.log("You Win!") 
+    alert("You Win!") 
     } else if (playerSelection === "paper" && computerSelection === "rock") {
         playerScore++;
-        console.log("You Win!") 
+        alert("You Win!")
     } else {
-        console.log("Invalid Input")
+    alert("Invalid Input")
     }
 }
 
-const playerSelection = "scissors"; // Player Choice
+const playerSelection = prompt("Rock, Paper or Scissors?"); // Player Choice
 const computerSelection = getComputerChoice(); // Computer
+
+
 console.log(playRound(playerSelection, computerSelection));
